9a5127e9b38b14efaebd3fbd1f4b9dcf45fb5bcf
Walter Banda <walterkaimurima@gmail.com>
2021-09-12 21:47:56 +0300

    Populated the Spinner 💫 👨🏿‍✈️

--- app/src/main/java/practice/learning/pluralsightnotty/NoteActivity.java
+++ app/src/main/java/practice/learning/pluralsightnotty/NoteActivity.java
@@ -1,11 +1,16 @@
 package practice.learning.pluralsightnotty;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
+import practice.learning.pluralsightnotty.adapters.DataManager;
 import practice.learning.pluralsightnotty.databinding.ActivityNoteBinding;
+import practice.learning.pluralsightnotty.models.CourseInfo;
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -22,5 +27,14 @@ public class NoteActivity extends AppCompatActivity {
 
 //        Layout 🏗 References
         Spinner courseSpinner = findViewById(R.id.spinner_courses);
+
+//        Course References and Variables 📦
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        ArrayAdapter<CourseInfo> adapterCourses =
+                new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
+
+//        Building 🏗 the spinner️
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        courseSpinner.setAdapter(adapterCourses);
     }
 }
